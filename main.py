a1b29be2e4e93beb39dd72fbfca28369779147de
ABekSo <45714936+ABekSo@users.noreply.github.com>
2018-12-08 20:13:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -724,7 +724,7 @@ if 'HEROKU' in list(os.environ.keys()):
     @server.route('/')
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url='https://my-money-bot.herokuapp.com/' + TOKEN)
+        bot.set_webhook(url='https://abekso.herokuapp.com/' + TOKEN)
         return '!', 200
     if __name__ == '__main__':
         db_acces.init_db()
